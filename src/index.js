fd74736dacbef236fd21548aa9116a8a58a40523
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2025-04-03 21:42:10 -0400

    Display list of produce.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import { populateProduce } from "./store/produce";
+import "./index.css";
+import App from "./App";
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.populateProduce = populateProduce;
+}
+
+const root = createRoot(document.getElementById("root"));
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
